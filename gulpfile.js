e3ba3207deb7b08aa92abcffa587d2ece4cf5bb3
Steve Aschwanden <steve.aschwanden@bluewin.ch>
2019-12-02 17:50:58 +0100

    Replace variables in gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var uglify = require('gulp-uglify-es').default;
 var minifycss = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 var imagemin = require('gulp-imagemin');
+var replace = require('gulp-replace');
 var pump = require('pump');
 var Hexo = require('hexo');
 
@@ -19,6 +20,24 @@ function exec_hexo(fn, args, cb) {
 
 var hexo = new Hexo(process.cwd(), {});
 inhex = hexo.init();
+
+gulp.task('set_variables', (cb) => {
+  pump([
+    gulp.src(['themes/icarus/_config.yml']),
+    replace('GULP_GITALK', '9992e6cc1af00d3d93f29c54bafba47ccc8df155'),
+    replace('GULP_GA', 'UA-146513464-1'),
+    gulp.dest('themes/icarus')
+  ], cb);
+});
+
+gulp.task('reset_variables', (cb) => {
+  pump([
+    gulp.src(['themes/icarus/_config.yml']),
+    replace('9992e6cc1af00d3d93f29c54bafba47ccc8df155', 'GULP_GITALK'),
+    replace('UA-146513464-1', 'GULP_GA'),
+    gulp.dest('themes/icarus')
+  ], cb);
+});
 
 gulp.task('hexo-clean', (cb) => { exec_hexo('clean', {}, cb); })
 
@@ -76,6 +95,6 @@ gulp.task('minify-img', (cb) => {
 
 gulp.task('compress', gulp.series('js-compress', 'minify-css', 'minify-html', 'minify-img'));
 
-gulp.task('generate', gulp.series('hexo-clean', 'hexo-generate'));
+gulp.task('generate', gulp.series('set_variables', 'hexo-clean', 'hexo-generate', 'reset_variables'));
 
 gulp.task('default', gulp.series('generate', 'compress'));
